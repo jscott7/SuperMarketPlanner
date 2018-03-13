c7c1f6f7b4908a1cf01509eb30d09c3372c9b759
jscott7 <jonathanscott8095@gmail.com>
2018-03-13 21:50:04 +0000

    Web Server update
    POST and GET dated files. Default behaviour is to get the current file

--- Server/RestServer.py
+++ Server/RestServer.py
@@ -1,26 +1,48 @@
 import web, urllib
 
+# Imports for finding latest file
+import glob
+import os
+
 urls = (
 	'/index', 'index'
 )
 
 class index:
     def GET(self):
-        x = web.input()
-        return "<b>text=<b>"
+        x = web.input(date="latest")
+        filedir = '/home/pi/data'
+        filename = filedir
+
+        if x.date == 'latest':
+           list_of_files = glob.glob( filedir + '/*' )
+           latest_file = max(list_of_files, key=os.path.getctime)
+           filename = latest_file 
+        else:
+           filename = filedir + '/' + x.date + '.xml'
+
+        try:
+            with open(filename, 'r') as myfile:
+                output = myfile.read()
+        except:
+            output = filename + ' is missing'
+
+        return output
 
     def POST(self):
+       
         # This is the xml string passed from the client
         x = web.data()
         
-		# Remove the header "data="
-        cleaned = x[5:]
-          
+	# Remove the header "data="
+        cleaned = x[9:]
+        date = x[:8]
+   
         # Write the XML string to file
-        filedir = '/home/pi/Scratch/test'
-        fout = open(filedir + '/test.xml', 'w')
+        filedir = '/home/pi/data'
+        fout = open(filedir + '/' + date +'.xml', 'w')
 
-		# Decode url included characters
+	# Decode url included characters
         fout.write(urllib.unquote_plus(cleaned))
         fout.close()
         return x
