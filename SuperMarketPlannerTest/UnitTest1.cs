8f604207b541fd51ec62f64342c0989bf7458c88
Jonathan Scott <jonathanscott8095@gmail.com>
2019-01-18 20:20:47 +0000

    Update Build targets and unit test

---
+++ SuperMarketPlannerTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperMarketPlanner;
+
+namespace SuperMarketPlannerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SelectedMeal selectedMeal = new SelectedMeal();
+            selectedMeal.DateTime = new DateTime(2018, 10, 11, 15, 00, 00, 000);
+
+            Assert.AreEqual("Thursday Oct 11", selectedMeal.Date);
+        }
+    }
+}
