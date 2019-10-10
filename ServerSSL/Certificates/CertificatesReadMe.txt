e1fd953974fffcb6ff26db4f17c24c7dc1451529
Jonathan Scott <jonathanscott8095@gmail.com>
2019-10-10 20:23:49 +0100

    Add SSL version of webserver

---
+++ ServerSSL/Certificates/CertificatesReadMe.txt
@@ -0,0 +1,4 @@
+This folder needs to contain two files
+
+jsco-cert.crt
+jsco-cert.key (The private key)
---
+++ ServerSSL/RestServerSSL.py
@@ -0,0 +1,95 @@
+import web, urllib
+
+# Imports for finding latest file
+import glob
+import os
+
+import xml.etree.ElementTree as ET
+
+from cheroot.server import HTTPServer
+from cheroot.ssl.builtin import BuiltinSSLAdapter
+
+HTTPServer.ssl_adapter = BuiltinSSLAdapter(
+    certificate = './Certificates/jsco-cert.crt',
+    private_key = './Certificates/jsco-cert.key')
+
+urls = (
+	'/index', 'index',
+        '/meals', 'meals'
+)
+
+#Global dictionay of common items
+t_globals = dict(datestr = web.datestr,)
+
+# The rendering engine for html templates
+render = web.template.render('/home/pi/templates', globals=t_globals)
+
+# Add the engine to globals so it can reference itself
+render._keywords['globals']['render'] = render
+
+filedir = '/home/pi/data'
+
+
+class index:
+    def GET(self):
+        x = web.input(date="latest")
+        filename = filedir
+
+        if x.date == 'latest':
+           list_of_files = glob.glob( filedir + '/*' )
+           latest_file = max(list_of_files, key=os.path.getctime)
+           filename = latest_file 
+        else:
+           filename = filedir + '/' + x.date + '.xml'
+
+        try:
+            with open(filename, 'r') as myfile:
+                output = myfile.read()
+        except:
+            output = filename + ' is missing'
+
+        return output
+
+    def POST(self):
+       
+        # This is the xml string passed from the client
+        x = web.data()
+        
+	# Remove the header "data="
+        cleaned = x[9:]
+        date = x[:8]
+   
+        # Write the XML string to file
+        filedir = '/home/pi/data'
+        fout = open(filedir + '/' + date +'.xml', 'w')
+
+	# Decode url included characters
+        fout.write(urllib.unquote_plus(cleaned))
+        fout.close()
+        return x
+
+class meals:
+    def GET(self):
+        
+        list_of_files = glob.glob( filedir + '/*' )
+        latest_file = max(list_of_files, key=os.path.getctime)
+
+        tree = ET.parse(latest_file)
+        root = tree.getroot()
+
+        meals = []
+
+        for subelem in root.findall('ArrayOfSelectedMeal/SelectedMeal'):
+           # Do something with date here
+           date = subelem.find('DateTime')
+           meals.append(date.text)
+
+           for mealelem in subelem.findall('Meals/string'):
+              meals.append(mealelem.text)
+        #meals = ['meal1', 'meal2', 'meal3']
+        # Show page
+        return render.base(render.listing(meals))
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
